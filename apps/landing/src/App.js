d3eb6281d662ea5ff1af04299555da7a41d58c4b
Leigh Halliday <leighhalliday@gmail.com>
2020-09-20 14:27:57 -0400

    Two packages linked together

---
+++ apps/landing/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Thing } from "@shared/components";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Thing />
+      </header>
+    </div>
+  );
+}
+
+export default App;
